e0ab735e33ef373e19c30b3f5dea200f5cda70e2
Connor <connorfourt@gmail.com>
2018-06-26 08:54:47 -0700

    can now create tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180626043953) do
+ActiveRecord::Schema.define(version: 20180626153747) do
 
   create_table "tasks", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
