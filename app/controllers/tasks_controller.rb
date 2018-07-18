dcf4924747a1641be644cde7d9e8ddcea002a8e8
Connor <connorfourt@gmail.com>
2018-07-17 18:32:39 -0700

    got it workig, fuck

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,23 +28,23 @@ class TasksController < ApplicationController
 	def update
 		@task = Task.find(params[:id])
 	 
-	  if @task.update(task_params)
-	    redirect_to @task
-	  else
-	    render 'edit'
-	  end
+		if @task.update(task_params)
+			redirect_to @task
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
-	  @task = Task.find(params[:id])
-	  @task.destroy
-	 
-	  redirect_to tasks_path
+		@task = Task.find(params[:id])
+		@task.destroy
+
+		redirect_to tasks_path
 	end
 
 	private 
 	
 	def task_params
-		params.require(:task).permit(:title, :description)
+		params.require(:task).permit(:title, :description, :completed, :completed_at)
 	end
 end
