6c0d0fba354c73f7cdb3e8f718163f44d5087aa0
Connor <connorfourt@gmail.com>
2018-07-19 08:40:56 -0700

    more formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+	def back_link
+		link_to('<i class="back-arrow"></i>'.html_safe, :back, class: 'back-link')
+	end
 end
--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -5,12 +5,20 @@ module TasksHelper
 		if due_date.nil?
 			link_to 'Add due date', edit_task_path(task), class: 'subtle'
 		elsif due_date < Time.now && !task.completed
-			content_tag(:span, due_date, class: 'past-due', title: 'Task is past due')
+			content_tag(:span, due_date, class: 'text-danger', title: 'Task is past due')
 		elsif due_date.today? && !task.completed
-			content_tag(:span, due_date, class: 'coming-up', title: 'Task is due soon')
+			content_tag(:span, due_date, class: 'text-warning', title: 'Task is due soon')
 		else
 			content_tag(:span, due_date)
 		end
 		
+	end
+
+	def task_completed(task)
+		if task.completed?
+			"<span class='p-1 bg-success'><p class='text-white'><i class='fas fa-check'></i>Completed!</p></span>".html_safe
+		else
+			"<span class=''><p class='text-body'>Not complete</p></span>".html_safe
+		end
 	end
 end
